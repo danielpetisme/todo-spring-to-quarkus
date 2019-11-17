fc8cf6ca3d8a16d555fc85adba08586b9cf2a471
Clement Escoffier <clement.escoffier@gmail.com>
2019-11-17 09:45:27 +0100

    Simplify tests by switching to quarkus test

--- todo-app/src/test/java/io/sample/todoapp/TodoResourceTest.java
+++ todo-app/src/test/java/io/sample/todoapp/TodoResourceTest.java
@@ -1,171 +1,119 @@
 package io.sample.todoapp;
 
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
+import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.common.mapper.TypeRef;
-import io.restassured.http.ContentType;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.Validator;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
-import static junit.framework.TestCase.assertEquals;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 
-@Testcontainers
-@SpringBootTest(classes = TodoApplication.class)
+@QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TodoResourceTest {
 
-  @Container
-  public static final PostgreSQLContainer DATABASE = new PostgreSQLContainer<>()
-    .withDatabaseName("todo")
-    .withUsername("todo")
-    .withPassword("todo")
-    .withExposedPorts(5432)
-    .withCreateContainerCmdModifier(cmd ->
-      cmd
-        .withHostName("localhost")
-        .withPortBindings(new PortBinding(Ports.Binding.bindPort(5432), new ExposedPort(5432)))
-    );
+    @Test
+    @Order(1)
+    void testInitialItems() {
+        List<Todo> todos = get("/api").then()
+            .statusCode(HttpStatus.SC_OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .extract().body().as(getTodoTypeRef());
+        Assertions.assertEquals(4, todos.size());
 
-  @Autowired
-  private TodoRepository todoRepository;
+        get("/api/1").then()
+            .statusCode(HttpStatus.SC_OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body("title", is("Introduction to Quarkus"))
+            .body("completed", is(true));
+    }
 
-  @Autowired
-  private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+    @Test
+    @Order(2)
+    void testAddingAnItem() {
+        Todo todo = new Todo();
+        todo.setTitle("testing the application");
+        given()
+            .body(todo)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/api")
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body("title", is(todo.getTitle()))
+            .body("completed", is(false))
+            .body("id", is(5));
 
-  @Autowired
-  private Validator validator;
+        List<Todo> todos = get("/api").then()
+            .statusCode(HttpStatus.SC_OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .extract().body().as(getTodoTypeRef());
+        Assertions.assertEquals(5, todos.size());
 
-  @BeforeEach
-  public void setup() {
-    RestAssuredMockMvc.standaloneSetup(
-      MockMvcBuilders.standaloneSetup(new TodoResource(todoRepository))
-        .setMessageConverters(jacksonMessageConverter)
-        .setValidator(validator)
-    );
-    RestAssured.requestSpecification = new RequestSpecBuilder()
-      .setContentType(ContentType.JSON)
-      .setAccept(ContentType.JSON)
-      .build();
-  }
+    }
 
-  @Test
-  @Order(1)
-  void testInitialItems() {
-    List<Todo> todos = get("/api").then()
-      .statusCode(HttpStatus.SC_OK)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .extract().body().as(getTodoTypeRef());
-    assertEquals(4, todos.size());
+    @Test
+    @Order(3)
+    void testUpdatingAnItem() {
+        Todo todo = new Todo();
+        todo.setTitle("testing the application (updated)");
+        todo.setCompleted(true);
+        given()
+            .body(todo)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .patch("/api/{id}", 5)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body("title", is(todo.getTitle()))
+            .body("completed", is(true))
+            .body("id", is(5));
+    }
 
-    get("/api/1").then()
-      .statusCode(HttpStatus.SC_OK)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .body("title", is("Introduction to Quarkus"))
-      .body("completed", is(true));
-  }
+    @Test
+    @Order(4)
+    void testDeletingAnItem() {
+        given()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .delete("/api/{id}", 5)
+            .then()
+            .statusCode(HttpStatus.SC_NO_CONTENT);
 
-  @Test
-  @Order(2)
-  void testAddingAnItem() {
-    Todo todo = new Todo();
-    todo.setTitle("testing the application");
-    given()
-      .body(todo)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-      .when()
-      .post("/api")
-      .then()
-      .statusCode(HttpStatus.SC_CREATED)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .body("title", is(todo.getTitle()))
-      .body("completed", is(false))
-      .body("id", is(5));
+        List<Todo> todos = get("/api").then()
+            .statusCode(HttpStatus.SC_OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .extract().body().as(getTodoTypeRef());
+        Assertions.assertEquals(4, todos.size());
+    }
 
-    List<Todo> todos = get("/api").then()
-      .statusCode(HttpStatus.SC_OK)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .extract().body().as(getTodoTypeRef());
-    assertEquals(5, todos.size());
+    @Test
+    @Order(5)
+    void testDeleteCompleted() {
+        delete("/api")
+            .then()
+            .statusCode(HttpStatus.SC_NO_CONTENT);
 
-  }
+        List<Todo> todos = get("/api").then()
+            .statusCode(HttpStatus.SC_OK)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .extract().body().as(getTodoTypeRef());
+        Assertions.assertEquals(3, todos.size());
+    }
 
-  @Test
-  @Order(3)
-  void testUpdatingAnItem() {
-    Todo todo = new Todo();
-    todo.setTitle("testing the application (updated)");
-    todo.setCompleted(true);
-    given()
-      .body(todo)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-      .when()
-      .patch("/api/{id}", 5)
-      .then()
-      .statusCode(HttpStatus.SC_OK)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .body("title", is(todo.getTitle()))
-      .body("completed", is(true))
-      .body("id", is(5));
-  }
-
-  @Test
-  @Order(4)
-  void testDeletingAnItem() {
-    given()
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-      .when()
-      .delete("/api/{id}", 5)
-      .then()
-      .statusCode(HttpStatus.SC_NO_CONTENT);
-
-    List<Todo> todos = get("/api").then()
-      .statusCode(HttpStatus.SC_OK)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .extract().body().as(getTodoTypeRef());
-    assertEquals(4, todos.size());
-  }
-
-  @Test
-  @Order(5)
-  void testDeleteCompleted() {
-    delete("/api")
-      .then()
-      .statusCode(HttpStatus.SC_NO_CONTENT);
-
-    List<Todo> todos = get("/api").then()
-      .statusCode(HttpStatus.SC_OK)
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .extract().body().as(getTodoTypeRef());
-    assertEquals(3, todos.size());
-  }
-
-  @NotNull
-  private TypeRef<List<Todo>> getTodoTypeRef() {
-    return new TypeRef<List<Todo>>() {
-      // Kept empty on purpose
-    };
-  }
+    private TypeRef<List<Todo>> getTodoTypeRef() {
+        return new TypeRef<List<Todo>>() {
+            // Kept empty on purpose
+        };
+    }
 
 }
